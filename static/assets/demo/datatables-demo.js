17fe1b3589da60797eb1ace1650643f7fd2da244
wesleymb <wesleyafdp@gmail.com>
2022-09-26 22:49:37 -0300

    ajuste para apresentacao

--- static/assets/demo/datatables-demo.js
+++ static/assets/demo/datatables-demo.js
@@ -1,7 +1,4 @@
-$(document).ready(function () {
-  $('#dataTable').DataTable({
-      language: {
-          url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json',
-      },
-  });
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable').DataTable();
 });
